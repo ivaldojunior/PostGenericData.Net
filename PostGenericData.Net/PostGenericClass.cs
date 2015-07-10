5655a5dd152a5a4f155772d493c48f6ae5d89468
ivaldojr <ivaldo.junior@spread.com.br>
2015-07-10 16:49:30 -0300

    ..

--- PostGenericData.Net/PostGenericClass.cs
+++ PostGenericData.Net/PostGenericClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,6 +15,11 @@ namespace PostGenericData.Net
         public PostGenericClass(string URI)
         {
             _URI = URI;
+        }
+
+        private static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
         }
 
         public void Post<T>(object obj,ref string resul)
@@ -39,9 +45,67 @@ namespace PostGenericData.Net
 
         }
 
-        private static object GetPropValue(object src, string propName)
+        public async Task PostAsync<T>(object obj)
         {
-            return src.GetType().GetProperty(propName).GetValue(src, null);
+
+            var prop = obj.GetType().GetProperties();
+            string param = "soft=postgenericClass.net";
+
+            for (int i = 0; i < prop.Count(); i++)
+            {
+                param += "&" + prop[i].Name + "=" + GetPropValue(obj, prop[i].Name);
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                Uri u = new Uri(_URI);
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                wc.UploadStringAsync(u, param);
+            }
+
+        }
+
+        public void PostJson<T>(object obj, ref string resul)
+        {
+            if (obj == null)
+                return;
+
+            var json = JsonConvert.SerializeObject(obj);
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                resul = wc.UploadString(_URI, "POST", json);
+            }
+       }
+
+        public void PostJsonAsync<T>(object obj)
+        {
+            if (obj == null)
+                return;
+
+            var json = JsonConvert.SerializeObject(obj);
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                Uri u = new Uri(_URI);
+                wc.UploadStringAsync(u, "POST", json);
+            }
+        }
+
+        public void PostJsonAndGetObject<T>(object obj, ref T resul)
+        {
+            if (obj == null)
+                return;
+
+            var json = JsonConvert.SerializeObject(obj);
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var resul_tmp = wc.UploadString(_URI, "POST", json);
+                resul = JsonConvert.DeserializeObject<T>(resul_tmp);
+            }
+
         }
 
     }
--- TestPostGenericData.Net/TestPost.cs
+++ TestPostGenericData.Net/TestPost.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PostGenericData.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace TestPostGenericData.Net
 {
     [TestClass]
     public class TestPost
     {
+        string uri = "http://localhost:10108/";
         [TestMethod]
         public void Test1()
         {
             Example c = new Example() { name = "Jack", age = 26 };
-            PostGenericClass p = new PostGenericClass("http://localhost:10108/");
+            PostGenericClass p = new PostGenericClass(uri);
             string resul = string.Empty;
             p.Post<Example>(c,ref resul);
-
             resul = resul.Replace(System.Environment.NewLine, string.Empty).Trim();
-
         }
+
+
+        [TestMethod]
+        public void TestAsync1()
+        {
+            Example c = new Example() { name = "Jack", age = 26 };
+            PostGenericClass p = new PostGenericClass(uri);
+            List<Task> tasks = new List<Task>();
+            tasks.Add(Task.Run(() => p.PostAsync<Example>(c)));
+ 
+        }
+
+        [TestMethod]
+        public void TestJson1()
+        {
+            Example c = new Example() { name = "Jack", age = 26 };
+            PostGenericClass p = new PostGenericClass(uri);
+            string resul = string.Empty;
+            p.PostJson<Example>(c, ref resul);
+            resul = resul.Replace(System.Environment.NewLine, string.Empty).Trim();
+        }
+
 
     }
 
