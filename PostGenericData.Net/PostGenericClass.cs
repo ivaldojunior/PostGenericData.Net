a73cb3fdf7084c6c1a7b8a91d0f52b333d9cd434
ivaldojr <ivaldo.junior@spread.com.br>
2015-07-10 11:30:02 -0300

    ..

---
+++ PostGenericData.Net/PostGenericClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostGenericData.Net
+{
+    public class PostGenericClass
+    {
+
+        public void Post()
+        {
+            string URI = "http://localhost:10108";
+            string myParameters = "param1=value1&param2=value2&param3=value3";
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string HtmlResult = wc.UploadString(URI, myParameters);
+                HtmlResult = HtmlResult.Replace(System.Environment.NewLine, string.Empty).Trim();
+            }
+
+        }
+    }
+}
---
+++ TestPostGenericData.Net/TestPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPostGenericData.Net
+{
+    [TestClass]
+    public class TestPost
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
